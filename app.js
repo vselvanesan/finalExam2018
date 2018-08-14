4ded2dd6eff1cad8b6aad1b071260e54f9976f30
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-08-14 10:20:40 -0400

    removed unnecessary components

--- app.js
+++ app.js
@@ -4,15 +4,8 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-
-// Routing
-let indexRouter = require('./routes/index');
-
 let app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,8 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-
-app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
